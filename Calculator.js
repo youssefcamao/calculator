6f725f8cbcf0a9de2e8767ca2020318f93d1824e
Youssef Sbai <youssef.sbai@camao.one>
2023-02-15 09:42:11 +0100

    added comments to the js file

--- Calculator.js
+++ Calculator.js
@@ -1,22 +1,29 @@
+// Get the HTML elements for the input box and buttons
 const inputbox = document.getElementById('inputbox');
 const calculateButton = document.getElementById('calculate-button');
 const keys = document.getElementsByClassName('key');
 
+// Add a click event listener to each button
 for (const key of keys) {
   key.addEventListener('click', handleKeyClick);
 }
 
+// Handle a button click event
 function handleKeyClick(event) {
   const selectedSign = event.target.textContent;
 
+  // Check which button was clicked
   switch (selectedSign) {
+    // If the "=" button was clicked, evaluate the expression in the input box
     case '=':
       if (inputbox.value !== "")
         inputbox.value = evaluateExpression(inputbox.value);
       break;
+    // If the "AC" button was clicked, clear the input box
     case 'AC':
       inputbox.value = '';
       break;
+    // Otherwise, add the clicked button's text to the input box
     default:
       inputbox.value += selectedSign;
       break;
@@ -31,7 +38,9 @@ function evaluateExpression(expression) {
   } else if (expression.includes('-')) {
     result = performSubtraction(expression);
   }
-  if (result === undefined) {
+
+  // If the expression doesn't include either sign, return the expression as it is
+  if (!result) {
     result = expression
   }
   return result;
